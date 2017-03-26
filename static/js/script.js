4b14a0d716ce3ea3a9127e20fb4b314888180d2f
Daniel Monteagudo <dmonte3210@gmail.com>
2017-03-25 21:53:13 -0400

    resizes nicer now

--- static/js/script.js
+++ static/js/script.js
@@ -20,6 +20,18 @@ var setup = function() {
     });
 };
 
-
+window.onresize = function() {
+    width = svg.node().getBoundingClientRect().width;
+    height = svg.node().getBoundingClientRect().height;
+    d3.selectAll(".label").each(function(d, i) {
+	var x = width/2 + ((i > 0) * -5) + ((i==0) * 5);;
+	var y = height/2 + ((i > 0) * -50) + ((i==0) * 50);
+	d3.select(this)
+	    .attr("x", x)
+	    .attr("y", y)
+	    .attr("font-size", 100)
+	    .attr("transform", "rotate(" + (90 + (180*i)) + ", " + x + ", " + y + ")");
+    });
+}
 
 window.onload = setup;
